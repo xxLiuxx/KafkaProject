40e9753aad66d609485efc7b57a896061e11211b
xxLiuxx <yuchen150211@gmail.com>
2021-12-29 17:22:09 -0800

    consumer created

---
+++ library-events-consumer/src/main/java/com/xxliuxx/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,27 @@
+package com.xxliuxx.libraryeventsconsumer.config;
+
+import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+/**
+ * @author Yuchen Liu
+ */
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
+      ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
+      ConsumerFactory<Object, Object> kafkaConsumerFactory) {
+    ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+    configurer.configure(factory, kafkaConsumerFactory);
+    factory.setConcurrency(3);
+    return factory;
+  }
+}
